05beaf103fca56242bed6006fc00826e0c819da8
livymio <128103300+livymio@users.noreply.github.com>
2023-04-04 20:40:42 -0400

    Readability changes
    Changed variable names and added psuedocode to increase readability.

--- counter.py
+++ counter.py
@@ -9,9 +9,10 @@ config.read(file)
 class Setup:
 
     def __init__(self):
-        
-        self.x = 0
 
+        #defines variable x
+        self.countVar = 0
+        
         #layout of setup window
         self.root = tk.Tk()
         self.root.geometry("500x300")
@@ -37,24 +38,26 @@ class Setup:
 
         self.root.mainloop()
 
+    #function of load button
     def load(self):
-        
+        #function of count button
         def count():
-            self.x += 1
-            self.label2.config(text=self.x)
-
+            self.countVar += 1
+            self.label2.config(text=self.countVar)
+        #grabs title from config
         global title
         title = (config['SAVE 1']['TITLE'])
 
+        #layout of counter window
         self.load_window = tk.Toplevel()
         self.load_window.geometry("500x200")
         self.load_window['background']='#525252'
+
         self.load_window.title(config['SAVE 1']['TITLE'])
-        self.load_window.title(config['SAVE 1']['TITLE'])
-        self.x = int(config['SAVE 1']['COUNT']) 
+        self.countVar = int(config['SAVE 1']['COUNT']) 
         self.label = tk.Label(self.load_window, text=(config['SAVE 1']['TITLE']), font=('Arial', 18), fg='white', bg='#525252')
         self.label.pack(padx=10)
-        self.label2 = tk.Label(self.load_window, text=self.x, font=('Arial', 18), fg='white', bg='#525252')
+        self.label2 = tk.Label(self.load_window, text=self.countVar, font=('Arial', 18), fg='white', bg='#525252')
         self.label2.pack(padx=10)
         self.button = tk.Button(self.load_window, text="Encounters +1", font=('Arial', 18), fg='white', bg='#525252', command=count)
         self.button.pack(padx=10)
@@ -68,15 +71,15 @@ class Setup:
         if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
             self.root.destroy()
 
-    #save the textbox as var title and open counter
+    #save the textbox as config var title and open counter
     def save(self):
         global title
         title = self.textbox.get('1.0', tk.END)
         
         self.parent = self
         
-        global x
-        x = 0
+        global countVar
+        countVar = 0
         
         self.load_window = tk.Toplevel()
         self.load_window.geometry("500x200")
@@ -84,7 +87,7 @@ class Setup:
         self.load_window['background']='#525252'
         self.label = tk.Label(self.load_window, text=title, font=('Arial', 18), fg='white', bg='#525252')
         self.label.pack(padx=10)
-        self.label2 = tk.Label(self.load_window, text=x, font=('Arial', 18), fg='white', bg='#525252')
+        self.label2 = tk.Label(self.load_window, text=countVar, font=('Arial', 18), fg='white', bg='#525252')
         self.label2.pack(padx=10)
         self.button = tk.Button(self.load_window, text="Encounters +1", font=('Arial', 18), fg='white', bg='#525252', command=self.count)
         self.button.pack(padx=10)
@@ -95,19 +98,20 @@ class Setup:
 
     # increment x by 1
     def count(self):
-        self.x += 1
-        self.label2.config(text=self.x)
+        self.countVar += 1
+        self.label2.config(text=self.countVar)
 
+    #prompts user to safe on close
     def save_on_close(self):
         if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"):
             if messagebox.askyesno(title="Quit?", message="Do you want to save? This will overwrite any previous saves."):
-                if not config.has_section('SAVE 1'):
+                if not config.has_section('SAVE 1'): #if the config is empty or doesn't contain SAVE 1 it will write it
                     config.add_section('SAVE 1')
                 config.set('SAVE 1', 'TITLE', str(title))
-                config.set('SAVE 1', 'COUNT', str(self.x))
+                config.set('SAVE 1', 'COUNT', str(self.countVar))
                 with open(file, 'w') as f:
                     config.write(f)
             self.load_window.destroy()
             self.root.destroy()
 
-Setup() 
+Setup()
